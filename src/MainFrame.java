7d3494c2fb19bc1eebd00a917918cd2c278a2434
Jing Zhi <t.jingzhi1@gmail.com>
2015-12-22 00:42:34 +0800

    Initial commit - Project creation

---
+++ src/MainFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 2269971701250845501L;
+	public static void main(String[] args) {
+		
+	}
+}
