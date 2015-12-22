a46b06f8d8a3f78bdc9afb0b1ef8eb4bceab417d
Jing Zhi <t.jingzhi1@gmail.com>
2015-12-22 22:52:43 +0800

    Moved files

--- src/MainFrame.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-
-public class MainFrame extends JFrame {
-	private static final long serialVersionUID = 2269971701250845501L;
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/com/clicker/cat/MainFrame.java
@@ -0,0 +1,9 @@
+package com.clicker.cat;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 2269971701250845501L;
+	public static void main(String[] args) {
+		
+	}
+}
